2d805579f820b5be5e2220138d1df2bdc58e07c7
Ramakrushna-Karoshi <karoshiramakrushna@gmail.com>
2025-08-07 08:32:47 +0530

    Update monitor.py

--- monitor.py
+++ monitor.py
@@ -110,27 +110,109 @@
 
 #************************************************#
 
+# import sys
+# import time
+
+# # --- Functional: Pure validation functions ---
+
+# def is_temperature_ok(temp):
+#     return 95 <= temp <= 102
+
+# def is_pulse_rate_ok(pulse):
+#     return 60 <= pulse <= 100
+
+# def is_spo2_ok(spo2):
+#     return spo2 >= 90
+
+# def check_vitals(temp, pulse, spo2):
+#     if not is_temperature_ok(temp):
+#         return False, 'Temperature critical!'
+#     if not is_pulse_rate_ok(pulse):
+#         return False, 'Pulse Rate is out of range!'
+#     if not is_spo2_ok(spo2):
+#         return False, 'Oxygen Saturation out of range!'
+#     return True, None
+
+# # --- Aspect-Oriented: Alert decorator ---
+
+# def alert_on_failure(func):
+#     def wrapper(*args, **kwargs):
+#         ok, msg = func(*args, **kwargs)
+#         if not ok:
+#             print(msg)
+#             animate_alert()
+#         return ok
+#     return wrapper
+
+# # --- Procedural: Animation and main sequence ---
+
+# def animate_alert(duration=6, interval=1):
+#     for _ in range(duration):
+#         print('\r* ', end='')
+#         sys.stdout.flush()
+#         time.sleep(interval)
+#         print('\r *', end='')
+#         sys.stdout.flush()
+#         time.sleep(interval)
+#     print('\r  ', end='')
+
+# @alert_on_failure
+# def vitals_ok(temperature, pulse_rate, spo2):
+#     return check_vitals(temperature, pulse_rate, spo2)
+
+# # --- Object-Oriented: Encapsulate vitals ---
+
+# class Vitals:
+#     def __init__(self, temperature, pulse_rate, spo2):
+#         self.temperature = temperature
+#         self.pulse_rate = pulse_rate
+#         self.spo2 = spo2
+
+#     def status(self):
+#         return check_vitals(self.temperature, self.pulse_rate, self.spo2)
+
+#     def check_and_alert(self):
+#         ok, msg = self.status()
+#         if not ok:
+#             print(msg)
+#             animate_alert()
+#         return ok
+
+# # --- Main sequence ---
+
+# if __name__ == "__main__":
+#     # Example usage
+#     v = Vitals(101, 80, 95)
+#     if v.check_and_alert():
+#         print("All vitals are within range.")
+#     else:
+#         print("One or more vitals are out of range.")
+
+#**************************************************************#
+#**************************************************************#
+#**************************************************************#
+
+
 import sys
 import time
 
 # --- Functional: Pure validation functions ---
 
-def is_temperature_ok(temp):
-    return 95 <= temp <= 102
+def is_value_in_range(value, min_value, max_value):
+    """Check if value is within [min_value, max_value] inclusive."""
+    return min_value <= value <= max_value
 
-def is_pulse_rate_ok(pulse):
-    return 60 <= pulse <= 100
+def is_spo2_ok(spo2, min_value=90):
+    """Check if SpO2 is above or equal to min_value."""
+    return spo2 >= min_value
 
-def is_spo2_ok(spo2):
-    return spo2 >= 90
-
-def check_vitals(temp, pulse, spo2):
-    if not is_temperature_ok(temp):
-        return False, 'Temperature critical!'
-    if not is_pulse_rate_ok(pulse):
-        return False, 'Pulse Rate is out of range!'
-    if not is_spo2_ok(spo2):
-        return False, 'Oxygen Saturation out of range!'
+def check_vitals(temp, pulse, spo2, temp_range=(95, 102), pulse_range=(60, 100), spo2_min=90):
+    if not is_value_in_range(temp, *temp_range):
+        return False, f'Temperature out of range! ({temp_range[0]}-{temp_range[1]})'
+    if not is_value_in_range(pulse, *pulse_range):
+        return False, f'Pulse Rate out of range! ({pulse_range[0]}-{pulse_range[1]})'
+    if not is_spo2_ok(spo2, spo2_min):
+        return False, f'Oxygen Saturation out of range! (min {spo2_min})'
     return True, None
 
 # --- Aspect-Oriented: Alert decorator ---
@@ -157,19 +239,24 @@ def animate_alert(duration=6, interval=1):
     print('\r  ', end='')
 
 @alert_on_failure
-def vitals_ok(temperature, pulse_rate, spo2):
-    return check_vitals(temperature, pulse_rate, spo2)
+def vitals_ok(temperature, pulse_rate, spo2, temp_range=(95, 102), pulse_range=(60, 100), spo2_min=90):
+    return check_vitals(temperature, pulse_rate, spo2, temp_range, pulse_range, spo2_min)
 
 # --- Object-Oriented: Encapsulate vitals ---
 
 class Vitals:
-    def __init__(self, temperature, pulse_rate, spo2):
+    def __init__(self, temperature, pulse_rate, spo2,
+                 temp_range=(95, 102), pulse_range=(60, 100), spo2_min=90):
         self.temperature = temperature
         self.pulse_rate = pulse_rate
         self.spo2 = spo2
+        self.temp_range = temp_range
+        self.pulse_range = pulse_range
+        self.spo2_min = spo2_min
 
     def status(self):
-        return check_vitals(self.temperature, self.pulse_rate, self.spo2)
+        return check_vitals(self.temperature, self.pulse_rate, self.spo2,
+                            self.temp_range, self.pulse_range, self.spo2_min)
 
     def check_and_alert(self):
         ok, msg = self.status()
@@ -181,14 +268,11 @@ class Vitals:
 # --- Main sequence ---
 
 if __name__ == "__main__":
-    # Example usage
-    v = Vitals(101, 80, 95)
+    # Example usage with custom ranges
+    v = Vitals(101, 80, 95, temp_range=(96, 101), pulse_range=(65, 99), spo2_min=92)
     if v.check_and_alert():
         print("All vitals are within range.")
     else:
         print("One or more vitals are out of range.")
-
-
-
 
 
