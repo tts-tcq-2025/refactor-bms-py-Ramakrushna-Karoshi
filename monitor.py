51923b313ae12e52ced13faa97704ed118a15dea
Ramakrushna-Karoshi <karoshiramakrushna@gmail.com>
2025-10-04 23:34:04 +0530

    Update monitor.py

--- monitor.py
+++ monitor.py
@@ -1,21 +1,16 @@
 import sys
 import time
 
-# --- Functional: Pure validation functions ---
-
 def is_value_in_range(value, min_value, max_value):
-    """Check if value is within [min_value, max_value] inclusive."""
     return min_value <= value <= max_value
 
 def get_warning_ranges(min_value, max_value, tolerance_percent=1.5):
-    """Return near-low and near-high warning ranges based on tolerance percent."""
     tolerance = (tolerance_percent / 100) * max_value
     near_low = (min_value, min_value + tolerance)
     near_high = (max_value - tolerance, max_value)
     return near_low, near_high
 
-def check_vitals_with_warning(temp, pulse, spo2, temp_range=(95, 102), pulse_range=(60, 100), spo2_min=90):
-    # Temperature
+def check_temperature(temp, temp_range=(95, 102)):
     near_low, near_high = get_warning_ranges(*temp_range)
     if temp < temp_range[0]:
         return False, f'Temperature out of range! ({temp_range[0]}-{temp_range[1]})'
@@ -25,8 +20,9 @@ def check_vitals_with_warning(temp, pulse, spo2, temp_range=(95, 102), pulse_ran
         return True, 'Warning: Approaching hyperthermia'
     if temp > temp_range[1]:
         return False, f'Temperature out of range! ({temp_range[0]}-{temp_range[1]})'
+    return True, None
 
-    # Pulse Rate
+def check_pulse(pulse, pulse_range=(60, 100)):
     near_low, near_high = get_warning_ranges(*pulse_range)
     if pulse < pulse_range[0]:
         return False, f'Pulse Rate out of range! ({pulse_range[0]}-{pulse_range[1]})'
@@ -36,29 +32,26 @@ def check_vitals_with_warning(temp, pulse, spo2, temp_range=(95, 102), pulse_ran
         return True, 'Warning: Approaching tachycardia'
     if pulse > pulse_range[1]:
         return False, f'Pulse Rate out of range! ({pulse_range[0]}-{pulse_range[1]})'
+    return True, None
 
-    # SpO2
+def check_spo2(spo2, spo2_min=90):
     spo2_tolerance = (1.5 / 100) * spo2_min
     if spo2 < spo2_min:
         return False, f'Oxygen Saturation out of range! (min {spo2_min})'
     if spo2_min <= spo2 < spo2_min + spo2_tolerance:
         return True, 'Warning: Approaching hypoxemia'
-
     return True, None
 
-# --- Aspect-Oriented: Alert decorator ---
-
-def alert_on_failure(func):
-    def wrapper(*args, **kwargs):
-        ok, msg = func(*args, **kwargs)
-        if msg:
-            print(msg)
-            if not ok:
-                animate_alert()
-        return ok
-    return wrapper
-
-# --- Procedural: Animation and main sequence ---
+def check_vitals_with_warning(temp, pulse, spo2, temp_range=(95, 102), pulse_range=(60, 100), spo2_min=90):
+    for check_func, value, args in [
+        (check_temperature, temp, {'temp_range': temp_range}),
+        (check_pulse, pulse, {'pulse_range': pulse_range}),
+        (check_spo2, spo2, {'spo2_min': spo2_min}),
+    ]:
+        ok, msg = check_func(value, **args)
+        if not ok or msg:
+            return ok, msg
+    return True, None
 
 def animate_alert(duration=6, interval=1):
     for _ in range(duration):
@@ -70,11 +63,19 @@ def animate_alert(duration=6, interval=1):
         time.sleep(interval)
     print('\r  ', end='')
 
+def alert_on_failure(func):
+    def wrapper(*args, **kwargs):
+        ok, msg = func(*args, **kwargs)
+        if msg:
+            print(msg)
+            if not ok:
+                animate_alert()
+        return ok
+    return wrapper
+
 @alert_on_failure
 def vitals_ok(temperature, pulse_rate, spo2, temp_range=(95, 102), pulse_range=(60, 100), spo2_min=90):
     return check_vitals_with_warning(temperature, pulse_rate, spo2, temp_range, pulse_range, spo2_min)
-
-# --- Object-Oriented: Encapsulate vitals ---
 
 class Vitals:
     def __init__(self, temperature, pulse_rate, spo2,
@@ -98,10 +99,7 @@ class Vitals:
                 animate_alert()
         return ok
 
-# --- Main sequence ---
-
 if __name__ == "__main__":
-    # Example usage with custom ranges
     v = Vitals(100.6, 99, 92, temp_range=(96, 101), pulse_range=(65, 99), spo2_min=92)
     if v.check_and_alert():
         print("All vitals are within range or warning.")
