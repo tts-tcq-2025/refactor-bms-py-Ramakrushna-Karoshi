e6efb978472a8abecc985e5aa07a8d737e55c64f
Ramakrushna-Karoshi <karoshiramakrushna@gmail.com>
2025-10-05 00:26:59 +0530

    Update alerts.py

--- alerts.py
+++ alerts.py
@@ -1,1 +1,21 @@
+import sys
+import time
+
+def animate_alert(duration=6, interval=1):
+    for _ in range(duration):
+        print('\r* ', end='')
+        sys.stdout.flush()
+        time.sleep(interval)
+        print('\r *', end='')
+        sys.stdout.flush()
+        time.sleep(interval)
+    print('\r  ', end='')
+
+
+def alert_if_not_ok(msg, is_ok):
+    if not is_ok:
+        print(msg)
+        animate_alert()
+        return False
+    return True
 
