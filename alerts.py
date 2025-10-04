be97d0fcfa1f3b2958b4d9ec7483f88eb1a18fe4
Ramakrushna-Karoshi <karoshiramakrushna@gmail.com>
2025-10-05 01:00:31 +0530

    Update alerts.py

--- alerts.py
+++ alerts.py
@@ -1,7 +1,12 @@
 import sys
 import time
 
+
 def animate_alert(duration=6, interval=1):
+    """
+    Simple console animation for alerts.
+    Prints alternating '*' symbols for a given duration.
+    """
     for _ in range(duration):
         print('\r* ', end='')
         sys.stdout.flush()
@@ -13,6 +18,10 @@ def animate_alert(duration=6, interval=1):
 
 
 def alert_if_not_ok(msg, is_ok):
+    """
+    Print message and animate alert if status is not OK.
+    Returns False if alert triggered, else True.
+    """
     if not is_ok:
         print(msg)
         animate_alert()
